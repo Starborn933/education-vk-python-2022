64d7cc9cee86fe9a93c93aa80aa7a46338c746ae
Konstantin Ermakov <konstant.ermakov@yandex.ru>
2022-05-05 11:06:10 +0300

    add lection 19

---
+++ lection19 - CI-CD/lection19/code/tests/test.py
@@ -0,0 +1,20 @@
+
+
+def test_one():
+
+    assert 2 == 2
+
+
+def test_two():
+
+    assert 'abc' == 'abc'
+
+
+def test_three():
+
+    assert 5 > 2
+
+
+def test_four():
+
+    assert 'abc' != 'aaa'
